fb54952d5af6ae10efed8fb5dd028858c181ed69
bdiegel <bdiegel@gmail.com>
2015-03-03 19:25:34 -0500

    Updated the view location intent

--- app/src/main/java/com/example/diegelb/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/diegelb/sunshine/app/MainActivity.java
@@ -1,10 +1,8 @@
 package com.example.diegelb.sunshine.app;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -128,30 +126,30 @@ public class MainActivity extends ActionBarActivity implements ForecastFragment.
                startActivity(new Intent(this, SettingsActivity.class));
                break;
            }
-           case R.id.action_viewmap: {
-               showPreferredLocation();
-               break;
-           }
+//           case R.id.action_viewmap: {
+//               showPreferredLocation();
+//               break;
+//           }
        }
 
         return super.onOptionsItemSelected(item);
     }
 
-    public void showPreferredLocation() {
-        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
-        String postcode = sharedPref.getString(getString(R.string.pref_location_key),
-                getString(R.string.pref_location_default));
-
-        Uri uri = Uri.parse("geo:0,0?").buildUpon().appendQueryParameter("q", postcode).build();
-
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setData(uri);
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivity(intent);
-        } else {
-            Log.d(LOG_TAG, "Could not call: " + uri );
-        }
-    }
+//    public void showPreferredLocation() {
+//        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+//        String postcode = sharedPref.getString(getString(R.string.pref_location_key),
+//                getString(R.string.pref_location_default));
+//
+//        Uri uri = Uri.parse("geo:0,0?").buildUpon().appendQueryParameter("q", postcode).build();
+//
+//        Intent intent = new Intent(Intent.ACTION_VIEW);
+//        intent.setData(uri);
+//        if (intent.resolveActivity(getPackageManager()) != null) {
+//            startActivity(intent);
+//        } else {
+//            Log.d(LOG_TAG, "Could not call: " + uri );
+//        }
+//    }
 
     @Override
     public void onItemSelected(Uri contentUri) {
